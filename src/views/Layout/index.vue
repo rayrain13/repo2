dcde9ee02766064f33c3063031a4587feb1cd10d
rayrain13 <dirtin3@outlook.com>
2024-07-16 10:15:28 +0800

    feat:Layout页面框架搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,13 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
+</script>
+
+
 <template>
-    <div>
-        Layout
-        <RouterView />
-    </div>
+    <LayoutNav />
+    <LayoutHeader />
+    <RouterView />
+    <LayoutFooter />
 </template>
