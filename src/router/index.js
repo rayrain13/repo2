ec1eb03e5be6fcba8db593ee0e0b54347d5e8611
rayrain13 <dirtin3@outlook.com>
2024-08-13 11:00:51 +0800

    feat:一级分类&解决路由缓存问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/views/Login/index.vue'
-import Layout from '@/views/Layout/index.vue'
-import Home from '@/views/Home/index.vue'
-import Category from '@/views/Category/index.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Category/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: "/",
       component: Layout,
-      children:[
+      children: [
         {
-          path: '',
+          path: "",
           component: Home,
         },
         {
-          path: 'category',
+          path: "category/:id",
           component: Category,
-        }
-      ]
+        },
+      ],
     },
     {
-      path: '/login',
+      path: "/login",
       component: Login,
-    }
-  ]
-})
+    },
+  ],
+});
 
-export default router
+export default router;
