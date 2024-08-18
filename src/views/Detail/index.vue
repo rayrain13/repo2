ae3598573a6f6d202d014de98843f95cbdb4e673
rayrain13 <dirtin3@outlook.com>
2024-08-19 00:20:32 +0800

    feat:Sku组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -5,8 +5,6 @@ import { onMounted, ref } from 'vue';
 import DetailHot from './components/DetailHot.vue';
 
 
-
-
 const route = useRoute()
 const goods = ref({})
 const getGoods = async () => {
@@ -14,6 +12,10 @@ const getGoods = async () => {
     goods.value = res.result
 }
 onMounted(() => getGoods())
+
+const skuChange = (sku) => {
+    console.log(sku);
+}
 
 </script>
 
@@ -23,9 +25,11 @@ onMounted(() => getGoods())
             <div class="bread-container">
                 <el-breadcrumb separator=">">
                     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                    <el-breadcrumb-item :to="{ path: `/category/${goods.categories[1].id}` }">{{ goods.categories[1].name }}
+                    <el-breadcrumb-item :to="{ path: `/category/${goods.categories[1].id}` }">{{
+                        goods.categories[1].name }}
                     </el-breadcrumb-item>
-                    <el-breadcrumb-item :to="{ path: `/category/sub/${goods.categories[0].id}` }">{{ goods.categories[0].name }}
+                    <el-breadcrumb-item :to="{ path: `/category/sub/${goods.categories[0].id}` }">{{
+                        goods.categories[0].name }}
                     </el-breadcrumb-item>
                     <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
                 </el-breadcrumb>
@@ -36,38 +40,38 @@ onMounted(() => getGoods())
                     <div class="goods-info">
                         <div class="media">
                             <!-- 图片预览区 -->
-
+                            <XtxImageView :image-list="goods.mainPictures" />
                             <!-- 统计数量 -->
                             <ul class="goods-sales">
                                 <li>
                                     <p>销量人气</p>
-                                    <p> {{goods.salesCount}} </p>
+                                    <p> {{ goods.salesCount }} </p>
                                     <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                                 </li>
                                 <li>
                                     <p>商品评价</p>
-                                    <p>{{goods.commentCount}}</p>
+                                    <p>{{ goods.commentCount }}</p>
                                     <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                                 </li>
                                 <li>
                                     <p>收藏人气</p>
-                                    <p>{{goods.collectCount}}</p>
+                                    <p>{{ goods.collectCount }}</p>
                                     <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                                 </li>
                                 <li>
                                     <p>品牌信息</p>
-                                    <p>{{goods.brand.name}}</p>
+                                    <p>{{ goods.brand.name }}</p>
                                     <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                                 </li>
                             </ul>
                         </div>
                         <div class="spec">
                             <!-- 商品信息区 -->
-                            <p class="g-name"> {{goods.name}} </p>
-                            <p class="g-desc">{{goods.desc}} </p>
+                            <p class="g-name"> {{ goods.name }} </p>
+                            <p class="g-desc">{{ goods.desc }} </p>
                             <p class="g-price">
-                                <span>{{goods.oldPrice}}</span>
-                                <span> {{goods.price}}</span>
+                                <span>{{ goods.oldPrice }}</span>
+                                <span> {{ goods.price }}</span>
                             </p>
                             <div class="g-service">
                                 <dl>
@@ -85,7 +89,7 @@ onMounted(() => getGoods())
                                 </dl>
                             </div>
                             <!-- sku组件 -->
-
+                            <XtxSku :goods="goods" @change="skuChange" />
                             <!-- 数据组件 -->
 
                             <!-- 按钮组件 -->
@@ -108,8 +112,8 @@ onMounted(() => getGoods())
                                     <!-- 属性 -->
                                     <ul class="attrs">
                                         <li v-for="item in goods.details.properties" :key="item.value">
-                                            <span class="dt">{{item.name}}</span>
-                                            <span class="dd">{{item.value}}</span>
+                                            <span class="dt">{{ item.name }}</span>
+                                            <span class="dd">{{ item.value }}</span>
                                         </li>
                                     </ul>
                                     <!-- 图片 -->
@@ -119,8 +123,8 @@ onMounted(() => getGoods())
                         </div>
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
-                            <DetailHot :type="1"/>
-                            <DetailHot :type="2"/>
+                            <DetailHot :type="1" />
+                            <DetailHot :type="2" />
                         </div>
                     </div>
                 </div>
