373c710059cc8c5bccf488131d3f413b338edb6d
rayrain13 <dirtin3@outlook.com>
2024-08-28 13:00:25 +0800

    feat:token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import 'element-plus/theme-chalk/el-message.css'
 import { ElMessage } from 'element-plus'
+import { useUserStore } from "@/stores/user";
+import router from "@/router";
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -9,12 +11,29 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
-    return config
-  }, e => Promise.reject(e))
-  
+  // 1. 从pinia获取token数据
+  const userStore = useUserStore()
+  // 2. 按照后端的要求拼接token数据
+  const token = userStore.userInfo.token
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+}, e => Promise.reject(e))
+
   // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
-    ElMessage({type:'warning',message:e.response.data.message})
+  const userStore = useUserStore()
+    ElMessage({
+    type:'warning',
+    message:e.response.data.message
+  })
+
+  //401token失效处理
+  if(e.response.staus === 401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
     return Promise.reject(e)
   })
 
